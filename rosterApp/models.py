fffd1f1376c436e5cdc9ea4f39beab66d976d09b
Rodrigo Osorio <rodrigo.osorio@clickfono.com>
2023-06-19 23:56:58 +1000

    Add models: Steward, Kitchen, Availability, Assignment and TradingHour

--- rosterApp/models.py
+++ rosterApp/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
-# Create your models here.
+
+class Steward(models.Model):
+    name = models.CharField(max_length=100)
+    surname = models.CharField(max_length=100)
+    kitchens = models.ManyToManyField('Kitchen', through='Assignment')
+
+class Availability(models.Model):
+    days = models.TextField(max_length=100)
+    start_time = models.TimeField()
+    end_time = models.TimeField()
+    steward = models.OneToOneField('Steward', on_delete=models.CASCADE)
+
+
+class Kitchen(models.Model):
+    name = models.CharField(max_length=100)
+    head_chef = models.CharField(max_length=100)
+    trading_hours = models.ForeignKey('TradingHours', on_delete=models.CASCADE)
+
+
+class TradingHour(models.Model):
+    opening_hour = models.TimeField()
+    closing_hour = models.TimeField()
+
+
+class Assignment(models.Model):
+    date = models.DateField()
+    start_time = models.TimeField()
+    end_time = models.TimeField()
+    steward = models.ForeignKey(Steward, on_delete=models.CASCADE)
+    kitchen = models.ForeignKey(Kitchen, on_delete=models.CASCADE)
+
+
